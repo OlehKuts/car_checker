400375526acb14016d763a1488181166edd7cf69
OlehKuts <oleh.kuts@yahoo.com>
2024-12-09 16:56:11 +0200

    changes in CarItem

--- src/components/CarItem.jsx
+++ src/components/CarItem.jsx
@@ -1,64 +1,35 @@
-import { useState } from "react";
 import { Icon } from "./Icon";
 export const CarItem = ({ car, isAuth, idx }) => {
-  const {
-    section,
-    brand,
-    carNumber,
-    color = "white",
-    type,
-    fee,
-    appNum,
-    carAmount,
-  } = car;
-  const [isClicked, setIsClicked] = useState(false);
-  const clicker = () => {
-    if (!isAuth) return;
-    setIsClicked((prev) => !prev);
-  };
+  const { section, brand, carNumber, color = "white", type, fee, appNum } = car;
   return (
     <>
       <div
         className="car_line"
-        onClick={clicker}
         style={{
-          backgroundColor:
-            isAuth && isClicked
-              ? "lavender"
-              : idx % 2 === 0
-              ? "aliceblue"
-              : "white",
+          backgroundColor: idx % 2 === 0 ? "aliceblue" : "white",
         }}
       >
         <div id="section">{section}</div>
+        {isAuth ? <div id="section_2">{appNum}</div> : null}
         <div>{brand}</div>
         <div>{carNumber}</div>
         <div
+          id={isAuth ? "section_3" : ""}
           style={{
-            backgroundColor: isClicked
-              ? "lavender"
-              : type && color === "white"
-              ? "lightGrey"
-              : idx % 2 === 0
-              ? "aliceblue"
-              : "white",
+            backgroundColor:
+              type && color === "white"
+                ? "lightGrey"
+                : idx % 2 === 0
+                ? "aliceblue"
+                : "white",
           }}
         >
           <Icon name="hatchbackCar" color={color} size="30px" />
         </div>
+        {isAuth ? (
+          <div id="section_4">{fee > 0 ? `${fee}` : `2-е авто`}</div>
+        ) : null}
       </div>
-      {isAuth && isClicked ? (
-        <div
-          className="car_line"
-          style={{
-            backgroundColor: isAuth && isClicked ? "lavender" : "white",
-          }}
-        >
-          <div>#{appNum}</div>
-          <div>{fee} ₴</div>
-          <div>{carAmount} авто</div>
-        </div>
-      ) : null}
     </>
   );
 };
